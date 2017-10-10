2eafb6f920b2bfc4ffa48f5a7798ec08f84d9e56
Andre Sampaio <andre.sampaio@plotprojects.com>
2017-10-10 12:20:10 +0200

    Initial Commit

---
+++ landingpagepoc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  landingpagepoc
+//
+//  Created by Andre Sampaio on 10/10/2017.
+//  Copyright © 2017 Andre Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ landingpagepoc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  landingpagepoc
+//
+//  Created by Andre Sampaio on 10/10/2017.
+//  Copyright © 2017 Andre Sampaio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
